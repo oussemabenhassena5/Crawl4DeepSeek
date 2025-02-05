2461a9514cffd4d6fb7dad55a9f00a9383dff5b2
oussemabenhassena5 (github) <oussemabenhassena5@gmail.com>
2025-02-05 11:44:15 +0100

    config: add required fields and data model constants

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 BASE_URL = "https://www.theknot.com/marketplace/wedding-reception-venues-atlanta-ga"
 CSS_SELECTOR = "[class^='info-container']"
+
+REQUIRED_KEYS = [
+    "name",
+    "price",
+    "location",
+    "capacity",
+    "rating",
+    "reviews",
+    "description",
+]
